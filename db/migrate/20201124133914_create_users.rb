c20f1e95c4ef7dcda33fe015c338e31e2edfb6aa
Yanis763 <yanispsn76@hotmail.com>
2020-11-24 15:18:33 +0100

    devise

--- db/migrate/20201124133914_create_users.rb
+++ db/migrate/20201124133914_create_users.rb
@@ -4,7 +4,6 @@ class CreateUsers < ActiveRecord::Migration[5.2]
       t.string :first_name
       t.string :last_name
       t.string :email
-      t.references :blob
 
       t.timestamps
     end
