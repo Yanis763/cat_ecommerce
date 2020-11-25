b0ad80082979a393024526bbe5c2a723aad9fff3
Yanis763 <yanispsn76@hotmail.com>
2020-11-25 02:30:15 +0100

    petits changements ta vu

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  root to: "products#index"
   resources :orders
   resources :carts
   resources :products 
   devise_for :users
-  root to: "products#index"
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
