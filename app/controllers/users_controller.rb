d246c1c197a93a71f299a9fbcec124a5529ad281
NathanMUSENGER <nathan.musenger@gmail.com>
2020-11-25 17:38:27 +0100

    devise

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,15 +41,11 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if params[:user][:password].blank?
+      params[:user].delete(:password)
+      params[:user].delete(:password_confirmation)
     end
+    super
   end
 
   # DELETE /users/1
