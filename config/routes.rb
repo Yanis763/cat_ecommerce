1902471ba44f00a948056cdd4f7302bfa2316ed0
Yanis763 <yanispsn76@hotmail.com>
2020-11-25 02:25:48 +0100

    2.0

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :carts
   resources :products
   devise_for :users
-  root to: "users#index"
+  root to: "products#index"
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 require 'faker'
 
 # ****************************** Create 2 products ******************************
-2.times do
+20.times do
   Product.create(title: Faker::Artist.name, description:Faker::Creature::Cat.breed, price: Faker::Number.decimal(l_digits: 2))
 end
 tp Product.all
