1946977aee2b667c9587439166492738f5c7d8a5
Théophile <theophile.cueille@gmail.com>
2020-11-25 12:53:22 +0100

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,10 @@
 require 'faker'
+# ****************************** Create 2 Users ******************************
+2.times do
+  User.create(encrypted_password: Faker::Internet.password, first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email(domain: 'yopmail.com'))
+end
+tp User.all
+
 
 # ****************************** Create 2 products ******************************
 2.times do
@@ -6,8 +12,15 @@ require 'faker'
 end
 tp Product.all
 
+
+# ****************************** Create 2 carts ******************************
+2.times do
+  Cart.create(user: User.all.sample)
+end
+tp Cart.all
 # ****************************** Create 2 orders ******************************
 2.times do
-  Order.create(quantity: Faker::Number.between(from: 1, to: 10))
+  Order.create(cart: Cart.all.sample, product: Product.all.sample, quantity: Faker::Number.between(from: 1, to: 10))
 end
 tp Order.all
+
