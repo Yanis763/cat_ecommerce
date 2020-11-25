644df9d06f9afb719f6189e6bc9c1e54ee26ee5a
Théophile <theophile.cueille@gmail.com>
2020-11-26 00:13:55 +0100

    user devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,9 @@
 class User < ApplicationRecord
-  has_many :carts
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-  :recoverable, :rememberable, :validatable
-  validates :first_name, length: { minimum: 2 }
-  validates :last_name, length: { minimum: 2 }
-  validates :password, length: { in: 6..20 }
+         :recoverable, :rememberable, :validatable
+
+         has_one_attached :avatar
+         validates_presence_of :last_name, :first_name
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :orders
   resources :carts
   resources :products
-  devise_for :users
-  root to: "users#index"
-  resources :users
+  root to: "products#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
